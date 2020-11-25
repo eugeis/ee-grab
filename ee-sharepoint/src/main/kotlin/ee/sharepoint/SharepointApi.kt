a733b25f5dd2e06b59eb241f0b0cb40a19264728
Eugen Eisler <eoeisler@gmail.com>
2020-11-25 11:03:06 +0100

    Rework http router

--- ee-sharepoint/src/main/kotlin/ee/sharepoint/SharepointApi.kt
+++ ee-sharepoint/src/main/kotlin/ee/sharepoint/SharepointApi.kt
@@ -1,8 +1,7 @@
 package ee.sharepoint
 
-import com.google.common.collect.Maps
-import com.google.common.collect.Sets
 import ee.common.cr.waitFor
+import ee.common.ext.exists
 import ee.common.ext.safe
 import ee.grab.core.Browser
 import ee.grab.core.Page
@@ -11,9 +10,10 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.openqa.selenium.*
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxOptions
-import org.openqa.selenium.remote.CapabilityType
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.nio.file.Files
@@ -73,33 +73,67 @@ class Queue<T>(var items: MutableList<T> = mutableListOf()) {
     fun peek(): T? = items[0]
 }
 
-class SharepointGrab(val rootTarget: Path) {
+class SharepointGrab(val downloadTarget: Path, firefox: Boolean = true) {
     private val log = LoggerFactory.getLogger("SharepointGrab")
 
     private val browser: Browser
-    private val rootTargetFile = rootTarget.toFile()
+    private val rootTargetFile = downloadTarget.toFile()
 
     init {
+        val driver = if (firefox) {
+            val firefoxOptions = FirefoxOptions()
 
-        val profile = FirefoxOptions()
+            firefoxOptions.addPreference("browser.download.folderList", 2)
+            firefoxOptions.addPreference("browser.download.manager.showWhenStarting", false)
+            firefoxOptions.addPreference("browser.download.dir", downloadTarget.toRealPath().toString())
+            firefoxOptions.addPreference("browser.helperApps.neverAsk.openFile",
+                    "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel," +
+                            "image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+            firefoxOptions.addPreference("browser.helperApps.neverAsk.saveToDisk",
+                    "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel," +
+                            "image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+            firefoxOptions.addPreference("browser.helperApps.alwaysAsk.force", false)
+            firefoxOptions.addPreference("browser.download.manager.alertOnEXEOpen", false)
+            firefoxOptions.addPreference("browser.download.manager.focusWhenStarting", false)
+            firefoxOptions.addPreference("browser.download.manager.useWindow", false)
+            firefoxOptions.addPreference("browser.download.manager.showAlertOnComplete", false)
+            firefoxOptions.addPreference("browser.download.manager.closeWhenDone", false)
 
-        profile.addPreference("browser.download.folderList", 2)
-        profile.addPreference("browser.download.manager.showWhenStarting", false)
-        profile.addPreference("browser.download.dir", rootTarget.toRealPath().toString())
-        profile.addPreference("browser.helperApps.neverAsk.openFile",
-                "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel," +
-                        "image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
-        profile.addPreference("browser.helperApps.neverAsk.saveToDisk",
-                "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel," +
-                        "image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
-        profile.addPreference("browser.helperApps.alwaysAsk.force", false)
-        profile.addPreference("browser.download.manager.alertOnEXEOpen", false)
-        profile.addPreference("browser.download.manager.focusWhenStarting", false)
-        profile.addPreference("browser.download.manager.useWindow", false)
-        profile.addPreference("browser.download.manager.showAlertOnComplete", false)
-        profile.addPreference("browser.download.manager.closeWhenDone", false)
+            FirefoxDriver(firefoxOptions)
+        } else {
+            val options = ChromeOptions()
 
-        val driver = FirefoxDriver(profile)
+            // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
+            //AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+            options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+            options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+            //options.addArguments("--headless"); // only if you are ACTUALLY running headless
+            options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+            options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+            options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+            options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+            options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+
+
+            options.setCapability("browser.download.folderList", 2)
+            options.setCapability("browser.download.manager.showWhenStarting", false)
+            options.setCapability("browser.download.dir", downloadTarget.toRealPath().toString())
+            options.setCapability("browser.helperApps.neverAsk.openFile",
+                    "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel," +
+                            "image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+            options.setCapability("browser.helperApps.neverAsk.saveToDisk",
+                    "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel," +
+                            "image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+            options.setCapability("browser.helperApps.alwaysAsk.force", false)
+            options.setCapability("browser.download.manager.alertOnEXEOpen", false)
+            options.setCapability("browser.download.manager.focusWhenStarting", false)
+            options.setCapability("browser.download.manager.useWindow", false)
+            options.setCapability("browser.download.manager.showAlertOnComplete", false)
+            options.setCapability("browser.download.manager.closeWhenDone", false)
+
+            ChromeDriver(options)
+        }
+
         browser = Browser.new(driver)
 
         driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS)
@@ -149,37 +183,51 @@ class SharepointGrab(val rootTarget: Path) {
         return rootFolder
     }
 
-    fun download(rootFolder: SpFolder) {
-        rootFolder.download(rootTarget.toFile())
+    fun download(rootFolder: SpFolder, targetFolder: Path, deleteExists: Boolean = true,
+                 ignoreExtensions: Set<String> = emptySet()) {
+        rootFolder.download(targetFolder.toFile(), deleteExists, ignoreExtensions)
+        log.info("download finished of '{}'", rootFolder.name)
     }
 
-    fun SpFolder.download(target: File) {
+    fun SpFolder.download(target: File, deleteExists: Boolean = true, ignoreExtensions: Set<String>) {
         val folder = File(target, name)
         if (!folder.exists()) {
             folder.mkdir()
         }
 
         folders.forEach {
-            it.download(folder)
+            it.download(folder, deleteExists, ignoreExtensions)
         }
 
         files.forEach {
-            it.download(folder)
+            val ext = it.url.substringAfterLast(".").toLowerCase()
+            if (!ignoreExtensions.contains(ext)) {
+                it.download(folder, deleteExists)
+            } else {
+                log.info("don't download, filter out by extension {}.{}", it.name, ext)
+            }
         }
     }
 
-    fun SpFile.download(target: File) {
+    fun SpFile.download(target: File, deleteExists: Boolean) {
         val fileName = "$name.${url.substringAfterLast(".")}"
 
-        val targetFile = File(target, fileName).toPath()
+        val targetFile = File(target, fileName)
+        val targetFilePath = targetFile.toPath()
         val downloadedFile = File(rootTargetFile, fileName)
-        if (downloadedFile.exists()) {
+
+        if (downloadedFile.exists() || targetFilePath.exists()) {
+            if (!deleteExists) {
+                log.warn("the file exists already, skip it: {}", targetFilePath)
+                return
+            }
             downloadedFile.delete()
+            targetFile.delete()
         }
 
         GlobalScope.launch {
-            safe(log, "download $targetFile") {
-                log.info("download {}", targetFile)
+            safe(log, "download $targetFilePath") {
+                log.info("download {}", targetFilePath)
                 browser.get(url)
             }
         }
@@ -192,11 +240,11 @@ class SharepointGrab(val rootTarget: Path) {
         }
 
         if (downloaded) {
-            safe(log, "move $downloadedFile to $targetFile") {
-                Files.move(downloadedFile.toPath(), targetFile, StandardCopyOption.REPLACE_EXISTING)
+            safe(log, "move $downloadedFile to $targetFilePath") {
+                Files.move(downloadedFile.toPath(), targetFilePath, StandardCopyOption.REPLACE_EXISTING)
             }
         } else {
-            log.warn("can't download {}", targetFile)
+            log.warn("can't download {}", targetFilePath)
         }
     }
 }
