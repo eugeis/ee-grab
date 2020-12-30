bfb6b46f508746b4d69ad8de85a5fce00b17e46f
Eugen Eisler <eoeisler@gmail.com>
2020-12-30 18:33:02 +0100

    Add, Remove, Update children implemented

--- ee-sharepoint/src/test/kotlin/ee/sharepoint/SharepointMain.kt
+++ ee-sharepoint/src/test/kotlin/ee/sharepoint/SharepointMain.kt
@@ -6,20 +6,18 @@ import ee.grab.core.Browser
 import java.nio.file.Paths
 
 fun main() {
-    System.setProperty("webdriver.gecko.driver",
-            "/home/z000ru5y/dev/d/ee-grab/ee-grab/drivers/geckodriver");
-    val folderName = "SSWA12"
-    val url = "https://wse02.siemens.com/content/P0002864/SSWA/Shared Documents/Forms/AllItems.aspx?" +
-            "RootFolder=%2Fcontent%2FP0002864%2FSSWA%2FShared Documents" +
-            "%2FSSWA Participant Folders%2F$folderName"
+    System.setProperty("webdriver.gecko.driver", "/home/z000ru5y/dev/d/ee-grab/ee-grab/drivers/geckodriver");
+    System.setProperty("webdriver.chrome.driver", "/home/z000ru5y/dev/d/ee-grab/ee-grab/drivers/chromedriver");
+    val folderName = "Documents"
+    val url = "https://wse02.siemens.com/content/P0002864/SSWA/Shared%20Documents/Forms/AllItems.aspx?View=%7B2E5798D3%2DB8D9%2D408F%2DB4CE%2D6ED0E20BA0A0%7D"
 
-    val target = Paths.get("/home/z000ru5y/SSWA/")
+    val target = Paths.get("/home/z000ru5y/Documents/SSWA/download/")
 
     val json = ObjectMapper()
     val targetFile = target.resolve("${folderName}.json").toFile()
 
-    val grab = SharepointGrab(target)
-    grab.login(url, 20000)
+    val grab = SharepointGrab(Paths.get("/home/z000ru5y/Downloads"), false)
+    grab.login(url, 40000)
 
     val folder: SpFolder = if (!targetFile.exists()) {
         val folder = grab.browse(folderName,
@@ -29,6 +27,5 @@ fun main() {
     } else {
         json.readValue(targetFile)
     }
-    grab.download(folder)
-
+    grab.download(folder, target, false, setOf("wmv", "mp4", "mp3"))
 }
