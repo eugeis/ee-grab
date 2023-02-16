154d442a8c559675048507a378c113961614a030
Eugen Eisler <eoeisler@gmail.com>
2023-02-16 11:55:15 +0100

    Working state

--- ee-grab/src/main/kotlin/ee/grab/core/DownloadSupport.kt
+++
@@ -1,7 +0,0 @@
-package ee.grab.core
-
-import java.nio.file.Path
-
-interface DownloadSupport {
-    fun downloadFile(url: String, target: Path, fileName: String, fileExt: String, prefix: String = ""): String
-}
